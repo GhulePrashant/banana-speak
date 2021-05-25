0e905305de57a3463d135928c4a7729f0d2bf768
Prashant Ghule <ghuleprashant790@gmail.com>
2021-05-26 01:59:43 +0530

    style: added css and some other changes

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+
+function getTranlationUrl(text) {
+    return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+    console.log("Error occured.", error);
+    alert("Something wrong with the server. Try after some time.");
+}
 
 btnTranslate.addEventListener("click", function clickEventHandler() {
     // console.log("Clicked");
     // console.log(txtInput.value);
-    outputDiv.innerText = "ahdgha" + txtInput.value;
+    // outputDiv.innerText = "ahdgha" + txtInput.value;
+
+    var inputText = txtInput.value;
+
+    fetch(getTranlationUrl(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var transaltedText = json.contents.translated;
+            outputDiv.innerText = transaltedText;
+        })
+        .catch(errorHandler);
 })
